31da3de14d9fd765036d12e6de67b1afeee250db
acarreiragonzalez <acarreiragonzalez@eniac25.danielcastelao.org>
2014-11-14 12:19:47 +0100

    j

---
+++ src/boletin9/pkg2/Boletin92.java
@@ -0,0 +1,16 @@
+
+package boletin9.pkg2;
+
+
+public class Boletin92 {
+
+  
+    public static void main(String[] args) {
+        int numero1,numero2;
+        
+        Metodos lalala=new Metodos();
+        
+        lalala.suma();
+    }
+    
+}
