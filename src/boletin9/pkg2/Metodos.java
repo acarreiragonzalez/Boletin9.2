29b04f2fc223e8c51e72800745ae108d4f832922
acarreiragonzalez <acarreiragonzalez@danielcastelao.org>
2014-11-14 12:36:40 +0100

    Ahora si

--- src/boletin9/pkg2/Metodos.java
+++ src/boletin9/pkg2/Metodos.java
@@ -4,31 +4,20 @@ package boletin9.pkg2;
 import javax.swing.JOptionPane;
 
 public class Metodos {
-    int num1, num2,suma;
-    int producto;
+    int num1, num2,suma=0;
+    double producto=1;
     
-    public double darValor(){
-    String resposta=JOptionPane.showInputDialog("dame o valor do numero 1  ");
-            
-    double valor=Double.parseDouble(resposta);
-            
-    return valor;}
     
-    public double darValor2(){
-    String resposta=JOptionPane.showInputDialog("dame o valor do numero  ");
-            
-    double valor=Double.parseDouble(resposta);
-            
-    return valor;}
+
     
     public void suma(){
         for (int i = 9; i < 90; i++) {
-            int contador = i;
             
-           suma= contador+suma++;
+            
+           suma= i+suma;
             JOptionPane.showMessageDialog(null, "A suma comprendida entre 10 e 90 " +suma);
             
-             producto = 1+contador*producto++;
+             producto =i*producto;
             
             JOptionPane.showMessageDialog(null, "O producto vai sendo " +producto);
         }
